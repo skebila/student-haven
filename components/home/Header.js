47d10ab8c560e46c3e5e00f134baf0bc188a40be
Steven <bongamke@sheridancollege.ca>
2022-03-26 08:06:50 -0400

    header component

---
+++ components/home/Header.js
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Ionicons, FontAwesome } from 'react-native-vector-icons';
+
+
+const Header = () => {
+  return (
+      <View style={styles.container}>
+          <TouchableOpacity>
+            <Ionicons name="menu" style={styles.icon} />
+          </TouchableOpacity>
+        
+          <Text style={styles.logo}> Student Haven </Text>
+
+          <TouchableOpacity>
+            <FontAwesome name="gear" style={styles.icon} />
+          </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        color: 'white'
+    },
+    icon: {
+        color: 'white',
+        fontSize: 30
+    },
+    logo: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: "700",
+    }
+})
+
+export default Header
---
+++ components/home/data/users.js
@@ -0,0 +1,23 @@
+export const USERS = [
+    {
+        user: "Steven"
+    },
+    {
+        user: "Jainam"
+    },
+    {
+        user: "Mona"
+    },
+    {
+        user: "Usman"
+    },
+    {
+        user: "Brandon"
+    },
+    {
+        user: "Isaac"
+    },
+    {
+        user: "Sara"
+    }  
+]
