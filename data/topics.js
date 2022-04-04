6b02e644e8e983a30dcefa3bd8ddb4d401d29faa
Steven <bongamke@sheridancollege.ca>
2022-04-04 06:25:34 -0400

    topics dummy data

---
+++ data/topics.js
@@ -0,0 +1,23 @@
+export const TOPICS = [
+    {
+        topic: "Accomodation",
+        icon:''
+    },
+    {
+        topic: "Jobs",
+        icon:''
+    },
+    {
+        topic: "Room Mates",
+        icon:''
+    },
+    {
+        topic: "Events",
+        icon:''
+    },
+    {
+        topic: "Buddies",
+        icon:''
+    },
+    
+]
