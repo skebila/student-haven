25225125360417cde4bc9f257dc81cac30254ce6
Steven <bongamke@sheridancollege.ca>
2022-09-20 14:23:23 -0400

    Accommodation and Event Pages works

--- screens/EventPost.js
+++ screens/EventPost.js
@@ -1,14 +1,30 @@
 import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native'
 import BottomTabs from '../components/home/BottomTabs'
-import React from 'react'
 import { Ionicons } from 'react-native-vector-icons';
+import Post from '../components/home/Post';
+import React, { useEffect, useState } from 'react'
+import { ScrollView } from 'react-native-gesture-handler'
+import { db } from '../firebase';
 
 
 const EventPost = ({navigation}) => {
+  const [posts, setPosts] = useState([])
+
+  //sets the post that has been created by the user
+  useEffect(() => {
+    db.collectionGroup('posts').where('topic', '==', 'Events').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => doc.data())) 
+    })
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <Header navigation={navigation} />
-      <BottomTabs navigation={navigation}/>
+      <ScrollView>
+        {posts.map((post, index) => ( //gets the post, maps it and displays it on the app UI
+          <Post post={post} key={index} navigation={navigation}/> 
+        ))}
+      </ScrollView>
         </SafeAreaView>
   )
 }
