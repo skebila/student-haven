935009ac39f857787ab3b5b75b0efa78363d5ab1
usmanAfzalKhan <usmanafzalkhan01@gmail.com>
2022-09-21 23:27:07 -0400

    Usman Messages PageDesign Commit

--- screens/Messages.js
+++ screens/Messages.js
@@ -1,55 +1,103 @@
-import { View, Text, SafeAreaView, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import React from 'react'
 import BottomTabs from '../components/home/BottomTabs'
+import { Ionicons } from 'react-native-vector-icons';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Messages = ({ navigation }) => {
+//Made by Usman
+
+//the main code (where everything is placed throughout the page)
+const Messages = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <SearchContact />
       <NoMessage />
-      <BottomTabs navigation={navigation} />
-    </SafeAreaView>
+      <BottomTabs navigation={navigation}/>
+        </SafeAreaView>
   )
 }
 
+//Header code and its style
 const Header = () => {
   return (
-    <View style={styles.container}>
+      <View style={styles.headerContainer}>
       <Text
         style={{
           fontSize: 16,
           color: 'white',
           fontWeight: "900",
-          textAlign: 'center'
+          textAlign: 'center',
         }}>Messages</Text>
-    </View>
+
+      </View>
   )
 }
 
-//Usman when the user opens the Message first time after they sign up
+//The search bar and its placement
+const SearchContact = () => {
+    return (
+        <SafeAreaView>
+            <View style={styles.searchBarContainer}>
+                <TouchableOpacity>
+                    <Ionicons name='search' style={styles.searchIcon} />
+                </TouchableOpacity>
+                <TextInput placeholder={'Search from Contacts'}
+                    placeholderTextColor={'#CDD0CB'} style={styles.TextInput} />
+            </View>       
+                    
+        </SafeAreaView>
+    )
+}
+
 const NoMessage = () => {
   return (
-    <View style={styles.container}>
+      <View style={styles.headerContainer}>
       <Text
         style={{
-          fontSize: 16,
+          fontSize: 13,
           color: 'white',
-          fontWeight: "900",
-          textAlign: 'center'
-        }}>You do not have any messages yet</Text>
-      <View style={{ marginHorizontal: '10%' }}>
-        <Button title="Send Message to Someone" onPress={() => navigation.push('')} />
-      </View>
+          fontWeight: "bold",
+          textAlign: 'center',
+        }}>No Messages Found</Text>
 
-    </View>
+      </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'black',
+    container: {
+      backgroundColor: 'black',
     flex: 1,
+  },
+  headerContainer: {
+      marginHorizontal: 0,
+      color: 'white',
+      marginTop: 10,
+      padding: 10,
+    },
+
+    searchBarContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#444444',
+      borderRadius: 10,
+      paddingLeft: 10,
+      marginHorizontal: 20,
+      marginTop: 0,
+  },
+  TextInput: {
+      height: 40,
+      color: '#EEEEEE',
+      fontWeight: '400',
+      marginLeft: 5,
+      width: '100%',
+      fontSize: 16
+  },
+  searchIcon: {
+      fontSize: 25,
+      color: '#CDD0CB',
   }
 })
 
