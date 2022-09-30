293beba2e634e1d4849d5a5e2b212519992b055f
Steven <bongamke@sheridancollege.ca>
2022-09-30 16:43:50 -0400

    Delete image updated --Steven

--- screens/EditProfile.js
+++ screens/EditProfile.js
@@ -31,7 +31,7 @@ const deleteProfilePic = async () => {
         db.collection('users')
             .doc(firebase.auth().currentUser.email)
             .update({
-                profile_picture: 'https://cdn4.iconfinder.com/data/icons/general-office/91/General_Office_33-512.png',
+                profile_picture: 'https://cdn.iconfinder.com/stored_data/634088/128/png?token=1664569916-Lv80s4ISxsK%2BUrAXd2WUy091DTpnKPYY74G%2FLLlmIY0%3D',
             }).then(() => {
             console.log('Image Deleted!');
         })
