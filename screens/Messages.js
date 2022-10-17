588ece243183d5727f985d4d314623d4f8757e8f
usmanAfzalKhan <usmanafzalkhan01@gmail.com>
2022-10-18 03:08:48 +0500

    Messaging part

--- screens/Messages.js
+++ screens/Messages.js
@@ -1,20 +1,87 @@
-import { View, Button, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
-import React from 'react'
+import {
+    View,
+    Button,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput,
+    Image,
+    ScrollView,
+    RefreshControl
+} from 'react-native'
+import React, {useEffect, useState} from 'react'
 import BottomTabs from '../components/home/BottomTabs'
 import { Ionicons } from 'react-native-vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {db, firebase} from "../firebase";
+import Post from "../components/home/Post";
+import {Divider} from "react-native-elements/dist/divider/Divider";
 
 //Made by Usman
-
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 //the main code (where everything is placed throughout the page)
 const Messages = ({navigation}) => {
-  return (
+    const [chat, setChat] = useState([])
+    const [users, setUsers] = useState([])
+    const [refreshing, setRefreshing] = React.useState(false);
+    useEffect(() => {
+        db.collection('chat')
+            .where('from', '==', firebase.auth().currentUser.email)
+            .get()
+            .then(snapshot => {
+                // console.log(snapshot.docs.map(doc => doc.data()))
+                // setChat([...new Set((snapshot.docs.map(doc => doc.data())).map(item => item.from))])
+                db.collection('chat')
+                    .where('to', '==', firebase.auth().currentUser.email)
+                    .get()
+                    .then(snapshot1 => {
+                        setChat([...new Set([...((snapshot1.docs.map(doc => doc.data())).map(item => item.from)), ...((snapshot.docs.map(doc => doc.data())).map(item => item.to))])])
+                        setUsers([...new Set([...((snapshot1.docs.map(doc => doc.data())).map(item => item.from)), ...((snapshot.docs.map(doc => doc.data())).map(item => item.to))])])
+                    })
+                console.log(chat)
+            })
+    }, [refreshing])
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
+    const findUser = (name) => {
+        let temp = [];
+        for(let c of users){
+            if(c.includes(name)){
+                temp.push(c);
+            }
+        }
+        setChat(temp)
+    }
+    return (
     <SafeAreaView style={styles.container}>
       <Header />
-      <SearchContact />
-      <NoMessage navigation={navigation}/>
-      <BottomTabs navigation={navigation}/>
-        </SafeAreaView>
+        <View style={styles.searchBarContainer}>
+            <TouchableOpacity>
+                <Ionicons name='search' style={styles.searchIcon} />
+            </TouchableOpacity>
+            <TextInput placeholder={'Search from Contacts'}
+                       onChangeText={(value) => findUser(value)}
+                       placeholderTextColor={'#CDD0CB'} style={styles.TextInput} />
+        </View>
+        <ScrollView style={{marginTop: 20, marginBottom: 70}}
+        refreshControl={
+            <RefreshControl
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+            />
+        }>
+            <View style={styles.headerContainer}>
+                {chat.map((c, index) => (
+                    <Person navigation={navigation} to={c} />
+                ))}
+            </View>
+        </ScrollView>
+        <BottomTabs navigation={navigation}/>
+    </SafeAreaView>
   )
 }
 
@@ -34,37 +101,40 @@ const Header = () => {
   )
 }
 
-//The search bar and its placement
-const SearchContact = () => {
-    return (
-        <SafeAreaView>
-            <View style={styles.searchBarContainer}>
-                <TouchableOpacity>
-                    <Ionicons name='search' style={styles.searchIcon} />
-                </TouchableOpacity>
-                <TextInput placeholder={'Search from Contacts'}
-                    placeholderTextColor={'#CDD0CB'} style={styles.TextInput} />
-            </View>
 
-        </SafeAreaView>
+
+const Person = ({navigation, to}) => {
+    const [person, setPerson] = useState([])
+    useEffect(() => {
+        db
+            .collection('users')
+            .doc(to)
+            .onSnapshot(snapshot => {
+                setPerson(snapshot.data())
+            })
+    }, [])
+
+    return (
+        <View>
+            <TouchableOpacity
+                onPress={() => navigation.navigate('MessengerScreen', {email: to})}
+            >
+                <View style={{flexDirection:'row', justifyContent: 'flex-start', alignItems: 'center'}}>
+                    <Image // profile image
+                        source={{uri: person.profile_picture}} style={styles.postHeaderImage}/>
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            color: 'white',
+                            fontWeight: "bold",
+                            textAlign: 'center',
+                        }}>{person.username}</Text>
+                </View>
+            </TouchableOpacity>
+            <Divider style={{ marginTop: 20, opacity: .3 }} />
+        </View>
     )
 }
-
-const NoMessage = ({navigation}) => {
-  return (
-      <View style={styles.headerContainer}>
-      <Text
-        style={{
-          fontSize: 13,
-          color: 'white',
-          fontWeight: "bold",
-          textAlign: 'center',
-        }}>No Messages Found</Text>
-      <Button title="Send Message to Someone Interface" onPress={() => navigation.push("MessengerScreen")} />
-      </View>
-  )
-}
-
 
 const styles = StyleSheet.create({
     container: {
@@ -98,7 +168,16 @@ const styles = StyleSheet.create({
   searchIcon: {
       fontSize: 25,
       color: '#CDD0CB',
-  }
+  },
+    postHeaderImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        marginTop: 10,
+        marginRight: 30,
+        borderWidth: 1.6,
+        borderColor: '#E5E5E5',
+    },
 })
 
 export default Messages
--- screens/Messenger.js
+++ screens/Messenger.js
@@ -1,52 +1,171 @@
-import { View, Button, Text, StyleSheet, KeyboardAvoidingView, TouchableOpacity, TextInput } from 'react-native'
-import React from 'react'
+import {
+    View,
+    Button,
+    Text,
+    StyleSheet,
+    KeyboardAvoidingView,
+    TouchableOpacity,
+    TextInput,
+    ScrollView, Alert, RefreshControl
+} from 'react-native'
+import React, {useEffect} from 'react'
 import { useState } from 'react';
 import BottomTabs from '../components/home/BottomTabs'
 import { Ionicons } from 'react-native-vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {db, firebase} from "../firebase";
+import messages from "./Messages";
 
 //Made by Usman
 
+const onSend = async (to, message) => {
+    const current = (new Date()).toUTCString();
+    if(message === null || message === '')
+        return;
+    console.log(current);
+    try {
+        await db.collection('chat')
+            .add({
+                owner_uid: firebase.auth().currentUser.uid,
+                from: firebase.auth().currentUser.email,
+                to: to,
+                message: message,
+                sent_at: current,
+            })
+    } catch (error) {
+        Alert.alert('Unable to send message. Please try again.')
+    }
+}
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 //the main code (where everything is placed throughout the page)
-const Messenger = ({ navigation }) => {
-  return (
+const Messenger = ({ navigation, route }) => {
+    const { email } = route.params;
+    const [sent, setSent] = useState([])
+    const [receive, setReceive] = useState([])
+    const [user, setUser] = useState([])
+    const [refreshing, setRefreshing] = React.useState(false);
+    useEffect(() => {
+        // fetching user data from firebase
+        db
+            .collection('users')
+            .doc(email)
+            .onSnapshot(snapshot => {
+                setUser(snapshot.data())
+            })
+        db.collection('chat')
+            .where('from', '==', firebase.auth().currentUser.email)
+            .where('to', '==', email)
+            .get()
+            .then(snapshot1 => {
+                db.collection('chat')
+                    .where('to', '==', firebase.auth().currentUser.email)
+                    .where('from', '==', email)
+                    .get()
+                    .then(snapshot => {
+                        setSent(getMessages([...(snapshot.docs.map(doc => doc.data())), ...(snapshot1.docs.map(doc => doc.data()))]))
+                    })
+            })
+    }, [refreshing])
+    const getMessages = (array) => {
+        array.sort(function(a,b){
+            return new Date(a.sent_at) - new Date(b.sent_at);
+        });
+        console.log(array)
+        return array;
+    }
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
+
+    return (
     <SafeAreaView style={styles.container}>
-      <Header />
-      <Text
-        style={{
-          fontSize: 13,
-          color: 'white',
-          fontWeight: "bold",
-          textAlign: 'center',
-          marginTop: 200,
-        }}>No Messages Found</Text>
-      <TextMessage />
-      <BottomTabs navigation={navigation} />
+        <Header title={user.username}/>
+        <ScrollView style={{marginTop: 20, marginBottom: 70}}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={refreshing}
+                            onRefresh={onRefresh}
+                        />
+                    }>
+            {sent.map((message, index) => (
+                <View>
+                    <ChatMessage message={message} />
+                    <Text>{'\n'}</Text>
+                </View>
+            ))}
+        </ScrollView>
+        <TextMessage to={user.email} />
     </SafeAreaView>
   )
 }
-
-const TextMessage = () => {
-  return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior='padding'>
-      <View style={styles.headerContainer}>
+const  ChatMessage = ({message}) => {
+    const getLocal = (date) => {
+        const d = new Date(date).toLocaleString()
+        return d;
+    }
+    return (
+        <View style={[styles.talkBubble, {
+            flexDirection: message.from === firebase.auth().currentUser.email ? 'row-reverse' : 'row',
+        }]}>
+            <View style={[styles.talkBubbleSquare, {
+                marginHorizontal: 20,
+                flexDirection: 'column',
+                height: 50,
+                width: '70%',
+                backgroundColor: message.from === firebase.auth().currentUser.email ? '#075E54' : 'gray',
+            }]}>
+                <Text
+                    style={{
+                        fontSize: 16,
+                        color: 'white',
+                        fontWeight: "bold",
+                        paddingHorizontal: 10,
+                        textAlign: message.from === firebase.auth().currentUser.email ? 'right' : 'left',
+                        // marginTop: 200,
+                    }}>{message.message}</Text>
+                <Text
+                    style={{
+                        fontSize: 10,
+                        color: 'white',
+                        fontWeight: "bold",
+                        paddingTop: 20,
+                        paddingHorizontal: 10,
+                        textAlign: message.from === firebase.auth().currentUser.email ? 'left' : 'right',
+                        // marginTop: 200,
+                    }}>{getLocal(message.sent_at)}</Text>
+            </View>
+        </View>
+    )
+}
+const TextMessage = ({to}) => {
+    const [message, setMessage] = useState(null)
+    return (
+      <View style={styles.textContainer}>
         <TextInput
           style={styles.input}
           multiline={true}
           placeholder='Type Message...'
+          value={message === null ? '' : message}
+          onChangeText={(value) => setMessage(value)}
         />
         <View style={styles.button}>
-        <Button 
-          title="Send Message" 
-          onPress={() => navigation.push("MessengerScreen")} />
+        <TouchableOpacity
+            onPress={()=> {
+                onSend(to, message).then(r => setMessage(''))
+            }}
+        >
+            <Ionicons name='send' style={styles.icon} />
+        </TouchableOpacity>
       </View>
       </View>
-    </KeyboardAvoidingView>
   )
 }
 
 //Header code and its style
-const Header = () => {
+const Header = ({title}) => {
   return (
     <View style={styles.headerContainer}>
       <Text
@@ -55,14 +174,38 @@ const Header = () => {
           color: 'white',
           fontWeight: "900",
           textAlign: 'center',
-        }}>Messenger</Text>
+        }}>{title}</Text>
 
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+    talkBubble: {
+        backgroundColor: "transparent",
+    },
+    talkBubbleSquare: {
+        width: 120,
+        height: 80,
+        borderRadius: 10,
+    },
+    textContainer: {
+        backgroundColor: 'black',
+        height: 100,
+        paddingBottom: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+        alignItems: 'center',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        width: '100%',
+
+    },
+
+    container: {
     backgroundColor: 'black',
     flex: 1,
   },
@@ -73,23 +216,26 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   input: {
-    width: 400,
+    width: '80%',
     borderWidth: 2,
     borderColor: 'grey',
     borderRadius: 5,
     color: 'grey',
+    marginRight: 10,
+    paddingHorizontal: 10,
     backgroundColor: 'white',
     height: 65,
     fontSize: 15,
-    marginTop: 315,
   },
+    icon: {
 
+        color: '#CDD0CB',
+        fontSize: 65
+    },
   button: {
-    width: 119,
-    height: 50,
-    borderRadius: 2,
-    marginLeft: 275,
-
+    width: '20%',
+    height: 65,
+    borderRadius: 5,
   },
 })
 
